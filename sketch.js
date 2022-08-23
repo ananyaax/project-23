d3d325bfb1aefd2b9747fb90929fd7c584b6197c
ananyaax <89581412+ananyaax@users.noreply.github.com>
2022-08-23 15:59:24 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,71 +3,72 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var canvas;
-var palyer, playerBase, playerArcher;
-var arrow;
-var baseimage;
-var playerimage;
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+var balls=[];
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-
+  canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-
+  
   angleMode(DEGREES);
+  angle = 15;
 
-  var options = {
-    isStatic: true
-  };
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
 
-  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
-  World.add(world, playerBase);
-
-  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world,player)
-
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.position.y - 112,
-    120,
-    120
-  );
-
-  arrow = new PlayerArrow(
-    playerArcher.body.position.x,
-    playerArcher.body.position.y,
-    100,
-    10
-  );
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+  
 }
 
 function draw() {
-  background(backgroundImg);
-  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-  image(playerimage,player.position.x,player.position.y,50,180)
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
   Engine.update(engine);
 
-  playerArcher.display();
-  arrow.display();
 
-  if (keyCode === 32) {
-    // arrow.(playerArcher.body.angle);
-    // arrow.shoot(playerArcher.angle);
-     arrow.shoot(playerArcher.body.angle);
-    // arrow.shoot(playerArcher);
+
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+ 
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  for(var i = 0; i < balls.length; i++){
+    showCannonBalls(balls[i],i)
   }
+  cannon.display();
 
-  // Title
-  fill("#FFFF");
-  textAlign("center");
-  textSize(40);
-  text("EPIC ARCHERY", width / 2, 100);
+}
+
+function keyPressed(){
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y)
+    Matter.Body.setAngle(cannonBall.body,cannon.angle)
+    balls.push(cannonBall)
+  }
+}
+
+function showCannonBalls(ball){
+  if(ball){
+    ball.display()
+  }
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length-1].shoot();
+  }
 }
